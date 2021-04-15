a164ae63c1d86ad456f3b007e9acca7621758507
A. Jayanth Goud <anigounigoud@gmail.com>
2021-04-15 16:58:52 +0530

    hard coded queries

--- src/services/teacher.ts
+++ src/services/teacher.ts
@@ -16,25 +16,31 @@ export class TeacherService {
 
     public async create(teacher: Partial<Teacher>) {
 
-        const newTeacher = await this.teacherRepository.create({
-            id: teacher.id,
-            name: teacher.name,
-            gender: teacher.gender,
-        })
+        // const newTeacher = await this.teacherRepository.create({
+        //     id: teacher.id,
+        //     name: teacher.name,
+        //     gender: teacher.gender,
+        // })
 
-        return this.teacherRepository.save(newTeacher)
+        const newTeacher = await this.teacherRepository.query('insert into public."Teacher"(name, gender) values($1, $2) returning *',[ teacher.name, teacher.gender])
+        return newTeacher
+
+        // return this.teacherRepository.save(newTeacher)
+        // return this.teacherRepository.query('select * from public."Teacher" where id = $1', [ newTeacher.id ]);
     }
 
     public async getTeacher(teacherId: number) {
 
-        const teacher = await this.teacherRepository.findOne({
-            where: {
-                id: teacherId
-            },
-            relations: ['subject']
+        // const teacher = await this.teacherRepository.findOne({
+        //     where: {
+        //         id: teacherId
+        //     },
+        //     relations: ['subject']
 
-        })
+        // })
 
+        const teacher = await this.teacherRepository.query('select Teacher.id, Teacher.name, Teacher.gender, subject from public."Teacher" as Teacher left join public."Subject" as Subject on Teacher."id" = Subject."teacherId" where Teacher.id=$1', [ teacherId ])
+        console.log(teacher)
         return teacher;
     }
 
@@ -42,8 +48,9 @@ export class TeacherService {
 
         if (academicYearId === undefined) {
 
-            const teachers = await this.teacherRepository.find({ relations: ['subject'] });
+            // const teachers = await this.teacherRepository.find({ relations: ['subject'] });
 
+            const teachers = await this.teacherRepository.query('select Teacher.id, Teacher.name, Teacher.gender, subject from public."Teacher" as Teacher left join public."Subject" as Subject on Teacher."id" = Subject."teacherId"')
 
             if (teachers.length <= 0) {
                 throw new Error("There are no teachers");
@@ -53,11 +60,13 @@ export class TeacherService {
 
         } else {
 
-            const pastTeachers = await this.teacherArchiveRepository.find({
-                where: {
-                    academicYear: academicYearId
-                }, relations: ['subject']
-            })
+            // const pastTeachers = await this.teacherArchiveRepository.find({
+            //     where: {
+            //         academicYear: academicYearId
+            //     }, relations: ['subject']
+            // })
+
+            const pastTeachers = await this.teacherArchiveRepository.query('select Teacher_Archive.id, Teacher_Archive.name, Teacher_Archive.gender, subject from public."Teacher_Archive" as Teacher left join public."Subject_Archive" as Subject_Archive on Teacher_Archive."id" = Subject_Archive."teacherId"')
 
             if (pastTeachers.length <= 0) {
                 throw new Error("There are no teachers in this Year");
@@ -83,16 +92,19 @@ export class TeacherService {
 
     public async deleteTeacher(teacherId: number) {
 
-        const deleteTeacher = await this.teacherRepository.findOne({
-            where: {
-                id: teacherId
-            }
-        })
+        // const deleteTeacher = await this.teacherRepository.findOne({
+        //     where: {
+        //         id: teacherId
+        //     }
+        // })
+
+        const deleteTeacher = await this.teacherRepository.query(`SELECT id FROM public."Teacher" where id = $1`, [ teacherId ])
+        console.log(deleteTeacher)
 
         if (!deleteTeacher) {
             throw new Error("There is no Teacher with this ID");
         } else {
-            return this.teacherRepository.delete(deleteTeacher.id)
+            return this.teacherRepository.query('delete from public."Teacher" where id = $1',[ teacherId ])
         }
     }
 
